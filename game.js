763402c948fe5e37abfe310adbe8fa3b0a0c55b9
Connor Sullivan <connor.sullivan13@ku.edu>
2021-04-12 22:32:38 -0500

    Add white space in board
    "Punch out" the slots on the board where the pieces
    will go when a player makes a turn.

--- game.js
+++ game.js
@@ -14,8 +14,28 @@ let tick = () => {
 let splat = () => {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
+    context.globalCompositeOperation = 'source-over';
+
     context.fillStyle = "blue";
     context.fillRect(120, 150, 740, 635);
+
+    context.globalCompositeOperation = 'destination-out';
+
+    let xInit = 175;
+    let yInit = 205;
+    for(let i = 0; i < 6; i++) {
+        xInit = 175;
+        for(let j = 0; j < 7; j++) {
+            context.fillStyle = "black";
+            context.beginPath();
+            context.arc(xInit, yInit, 50, 0 , 2 * Math.PI);
+            context.stroke();
+            context.fill();
+            xInit += 105;
+        }
+        yInit += 105;
+    }
+    
 
     tick();
 }
